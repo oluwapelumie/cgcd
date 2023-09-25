64a312930e59dc096b1b7b6ee0c811ad1f534a7a
oluwapelumie <emmanueladediran85@gmail.com>
2023-09-25 14:37:42 -0700

    pk p

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'swiper/css/bundle';
+import "swiper/swiper-bundle.min.css";
 import "react-modal-video/css/modal-video.min.css";
 import "../assets/css/animate.min.css";
 import "../assets/css/azino-icons.css";
 import "../assets/css/fontawesome-all.min.css";
 import "../assets/css/main.css";
-import 'swiper/css/bundle';
-import 'swiper/css';
+import "swiper/swiper-bundle.min.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
