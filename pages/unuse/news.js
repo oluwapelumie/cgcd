ee223bc7f65f393fd2babf266a2009b1c47ff790
oluwapelumie <emmanueladediran85@gmail.com>
2023-10-02 06:37:41 -0700

    changes

--- pages/unuse/news.js
+++ pages/unuse/news.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Layout from "../components/layout";
-import HeaderOne from "../components/header/header-one";
-import StickyHeader from "../components/header/sticky-header";
-import PageHeader from "../components/page-header";
-import BlogPage from "../components/blog/blog-page";
-import Footer from "../components/footer";
+import Layout from "../../components/layout";
+import HeaderOne from "../../components/header/header-one";
+import StickyHeader from "../../components/header/sticky-header";
+import PageHeader from "../../components/page-header";
+import BlogPage from "../../components/blog/blog-page";
+import Footer from "../../components/footer";
 
 const News = () => {
   return (
